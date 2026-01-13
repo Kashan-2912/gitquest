2a09fcfbfbc15eb7c189c01d6796a12349ae4c4c
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-01-13 23:49:10 +0500

    feat: add GitHub stars count button to header; integrate Hugeicons for icon display

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Github } from "@hugeicons/core-free-icons";
+
+export async function StarsCount() {
+  "use cache";
+
+  const data = await fetch(
+    "https://api.github.com/repos/Kashan-2912/gitquest",
+    { next: { revalidate: 86400 } }
+  );
+  const json = await data.json();
+  const stars = json.stargazers_count;
+
+  return (
+    <span className="w-12 text-muted-foreground text-xs tabular-nums">
+      {stars >= 1000 ? `${(stars / 1000).toFixed(1)}k` : stars.toLocaleString()}
+    </span>
+  );
+}
 
 const Header = () => {
   return (
     <header className="absolute top-0 p-4 w-full flex items-center justify-between z-50">
-      <Link href="/" className="group relative inline-flex items-center justify-center">
+      <Link
+        href="/"
+        className="group relative inline-flex items-center justify-center"
+      >
         <div className="relative overflow-hidden rounded-full">
           <Image
             src="/github-creature-logo.png"
@@ -22,10 +45,32 @@ const Header = () => {
       </Link>
 
       <nav className="flex items-center gap-6">
-        <Link href="/summon" className="text-lg font-medium hover:text-primary transition-colors">
+        <div className="flex items-center gap-2">
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Kashan-2912/gitquest"
+          >
+            <Button
+              variant="ghost"
+              size="icon"
+              className="flex items-center gap-2 px-6 cursor-pointer text-foreground hover:text-primary hover:bg-transparent"
+            >
+              <HugeiconsIcon icon={Github} />
+              <StarsCount />
+            </Button>
+          </Link>
+        </div>
+        <Link
+          href="/summon"
+          className="text-lg font-medium hover:text-primary transition-colors"
+        >
           Summon
         </Link>
-        <Link href="/leaderboard" className="text-lg font-medium hover:text-primary transition-colors">
+        <Link
+          href="/leaderboard"
+          className="text-lg font-medium hover:text-primary transition-colors"
+        >
           Leaderboard
         </Link>
         <ModeToggle />
@@ -35,4 +80,3 @@ const Header = () => {
 };
 
 export default Header;
-
